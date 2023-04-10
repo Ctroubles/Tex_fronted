4b58a88d6e546a119080cd4d00229420215707bc
troubles <cesar.troubles@gmail.com>
2023-04-10 12:58:12 -0500

    store reconfig

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,12 @@
-import { configureStore, } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducer/reducer';
 
-const store = configureStore({
-    reducer : rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunkMiddleware),
-    devTools : process.env.NODE_Env !== 'production',
-});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
