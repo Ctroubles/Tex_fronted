7341c364e9d17cc7bc2ce6f82065e9e03a1a3878
troubles <cesar.troubles@gmail.com>
2023-04-13 19:31:31 -0500

    perfil avances

--- src/views/Perfil_usuaio/Perfil_usuaio.jsx
+++ src/views/Perfil_usuaio/Perfil_usuaio.jsx
@@ -2,12 +2,25 @@ import style from "./Perfil_usuaio.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import iconAdmiration from "../../assets/icons/admiration_icon_smokeWhite.png";
 import MisCompras from "./Perfil_usuario_components/MisCompras/MisCompras";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Perfil_usuario = ()=>{
 
     const {user} = useAuth0()
+    const [usuario, setUsuario] = useState({})
 
-    console.log(user);
+    useEffect(()=>{
+        const setting = async()=>{
+            const {data} = await axios.get(`/users`,{email:user.email}).catch(err=>alert(err))
+            if (data) setUsuario(data)   
+            console.log(data);
+        }
+        setting()
+       
+      },[])
+
+    // console.log(user);
     return(
         <div id={style.Perfil_usuario}>
            <div id={style.main}>
@@ -18,12 +31,12 @@ const Perfil_usuario = ()=>{
                         </div>                       
                         <div>
                             <h1>
-                                César rrr
+                                {usuario.name}
                             </h1>
                         </div>                        
                         <div>
                             <p>
-                                cesar@gmail.com
+                                {usuario.email}
                             </p>
                         </div>
                     </div> 
@@ -40,26 +53,26 @@ const Perfil_usuario = ()=>{
                     <div id={style.pointsSection}>
                         <label>
                             <span>TEXpoints: </span>
-                            <span>14759</span>
+                            <span>{usuario.texPoints}</span>
                         </label>
                     </div>
                     <div id={style.informationContainer}>
                         <h1>Información</h1>
                         <div id={style.userData}>
                             <div className={style.row}>
-                                <label><span>Nombres: </span>César Alfredo</label>
-                                <label><span>Apellidos:</span>Llajaruna González</label>
+                                <label><span>Nombres: </span>{usuario.name}</label>
+                                <label><span>Apellidos:</span>{usuario.surname}</label>
                             </div>
                             <div className={style.row}>
                                 <label><span>Ciudad: </span>Trujillo</label>
                                 <label><span>Dirección:</span>Jr. La piedra</label>
                             </div>   
                             <div className={style.row}>
-                                <label><span>Dni: </span>72674963</label>
-                                <label><span>Cumpleaños:</span>25 de Diciembre <span id={style.admirationBirthday}><img src={iconAdmiration} alt="" /></span></label>
+                                <label><span>Dni: </span>{usuario.DNI}</label>
+                                <label><span>Cumpleaños:</span>{usuario.birthday} <span id={style.admirationBirthday}><img src={iconAdmiration} alt="" /></span></label>
                             </div>
                             <div className={style.row}>
-                                <label><span>Celular:</span>902038984</label>
+                                <label><span>Celular:</span>{usuario.phoneNumber}</label>
                             </div> 
                       
                         </div>
